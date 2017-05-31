db9d4ed5ab078c4a7ea1dc4fcc18b2083a600492
Mike <Mike.yingling15@gmail.com>
2017-05-31 16:12:38 -0400

    trivia game still conceptual

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,44 +1,87 @@
 
 
 //object containing questions and answers
-var appObject = {
-	"Is Javascript easier than it looks?" : true,
-	"Is futurama better than the Big Bang Theory?" : true,
-	"Which of these scientists was in Futurama?" : "Stephen Hawking",
-	"Who starred in the hit 1955 'Rebel Without a Case'?" : "James Dean",
-	"Who is Winston's lover in '1984'?" : "Julia",
-	"Who wrote '1984'?" : "George Orwell", 
-	"Everybody Loves Hypnotoad." : true,
-	"What is the capital of Australia" : "Canberra",
-	"What was the first planet to be discovered in 1781" : "Uranus",
-	"Where would you expect to hear 'Roll Tide'?" : "Alabama",
-	"Where were Forrest Gump and Jenny from?" : "Greenbow, Alabama",
-	"All Glory to the Hypnotoad" : true,
-};
+var questionList = [
+
+		{ 	question: "Is Javascript easier than it looks?",
+			answer: true
+		},
+		{
+			question: "Is futurama better than the Big Bang Theory?",
+			answer : true
+		},
+		{
+			question:"Which of these scientists was in Futurama?",
+			answer : "Stephen Hawking"
+		},
+		{
+			question:"Who starred in the hit 1955 'Rebel Without a Case'?",
+			answer: "James Dean"
+		},
+		{	question:"Who is Winston's lover in '1984'?",
+			answer : "Julia"
+		},
+		{	question:"Who wrote '1984'?",
+			answer: "George Orwell"
+		} ,
+		{	question:"Everybody Loves Hypnotoad.",
+			answer: true
+		},
+		{
+			question:"What is the capital of Australia", 
+			answer: "Canberra"
+		},
+		{
+			question:"What was the first planet to be discovered in 1781", 
+			answer: "Uranus"
+		},
+		{	question:"Where would you expect to hear 'Roll Tide'?",
+			answer: "Alabama"
+		},
+		{	question:"Where were Forrest Gump and Jenny from?",
+			answer: "Greenbow, Alabama"
+		},
+		{	question:"All Glory to the Hypnotoad", 
+			answer: true}
+	];
+
+//questionList[0].question 
+//if radiosubmitted == questionList[0].answer 
+///onFormSubmit - jQuery to select element on form, selected element == questionList.[0].answer
+//get selectedradio button getvalue
 
 	//Declared Variables
 var property;
 var questionsTime;
-var x;
-var writeQuestion = (Object.getOwnPropertyNames(appObject));
+	function checkQnA() {
+	for (var i = 0; i < questionList.length; i++) {
+		console.log(questionList[i].question);
+		console.log(questionList[i].answer);
+	$("#chaseQuest").append(questionList[i].question);
+	}
+}	
+var writeQuestion = (Object.getOwnPropertyNames(questionList));
 	//This was 'called' as a test
-	console.log("question Array?? is made from writeQuestion: " + writeQuestion);
+	console.log("var writeQuestion = (Object.getOwnPropertyNames(questionList)); :" + writeQuestion);
 
 function getQ() {//I hope to use getQ to $.html the questions into the html on the fly
-	for (var i = 0; i < writeQuestion.length; i++)
-		console.log(writeQuestion[i]);
+	for (var i = 0; i < questionList.length; i++)
+		console.log(questionList[i]);
+
+	var nextQuestion = $("<div>").html(questionList[i]);
 	//This writes the question the html box
-	$("#chaseQuest").html(writeQuestion[i]);
+	
 };
 
 function displayQ() {
-	if (writeQuestion.indexOf("Is Javascript easier than it looks?") === writeQuestion[0])
+	if (questionList.indexOf("Is Javascript easier than it looks?") === writeQuestion[0])
 	console.log("holy shit"); //fun fact - I couldn't get it to holy shit
 	
 };
 
 
 //these 
-getQ();
-console.log(Object.entries(appObject));
-displayQ();
+//getQ();
+console.log(Object.entries(questionList));
+//displayQ();
+checkQnA();
